d7db5a384d8e23b6f4262e5a33f7feccb79a3fb2
FilovetsAlexandr <sohan.boxan@gmail.com>
2024-03-01 14:11:49 +0300

    tab bar done

---
+++ RickAndMorty/RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RMSettingsViewController.swift
+//  RickAndMorty
+//
+//  Created by Alexandr Filovets on 1.03.24.
+//
+
+import UIKit
+
+final class RMSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        title = "Settings"
+    }
+
+
+}
